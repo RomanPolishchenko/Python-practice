e7e9f2a076e80287e8308dd5eea4463766765e90
Roman Polishchenko <romanpolishchenko@knu.ua>
2018-04-18 19:32:24 +0300

    Solved 14.9 task.

--- Inheritance/14.9 (sorting)/main14_9.py
+++ Inheritance/14.9 (sorting)/main14_9.py
@@ -1,3 +1,10 @@
 from collections import deque
 
 sequence = deque()
+
+numbers = list(input('Enter your numbers: ').split())
+for i in range(len(numbers)):
+    sequence.appendleft(max(numbers))
+    numbers.remove(max(numbers))
+
+print('Sorted:', " ".join(sequence))
